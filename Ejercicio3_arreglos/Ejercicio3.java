53b0de159c57a3d44fb60d4af7b5c1930d54e1a5
Nicolas Tapasco <tapasconicolas13@gmail.com>
2023-08-04 18:46:11 -0500

    Ejercicio 3 corregido

--- Ejercicio3_arreglos/Ejercicio3.java
+++ Ejercicio3_arreglos/Ejercicio3.java
@@ -25,74 +25,99 @@ public class Ejercicio3 {
 
                 switch (opcion) {
                     case 1:
-                        if (numero_registros > 0) {
-                            System.out.print("--->   Ingresa tu nombre completo: ");
-                            entrada.nextLine(); 
-                            arreglo[100 - numero_registros] = entrada.nextLine();
-                            numero_registros--;
-                        } else {
-                            System.out.println("No se puede registrar un nuevo cliente.");
+
+                    if (numero_registros > 0) {
+                        System.out.print("--->   Ingresa tu nombre completo: ");
+                        entrada.nextLine(); 
+                        String nombre_completo = entrada.nextLine();
+                        boolean nombre_existente = false;
+                        
+                        for (int i = 0; i < 100 - numero_registros; i++) {
+                            if (arreglo[i] != null && arreglo[i].equalsIgnoreCase(nombre_completo)) {
+                                nombre_existente = true;
+                                break;
+                            }
                         }
-                        break;
+                        
+                        if (nombre_existente) {
+                            System.out.println("El nombre ya esta en la lista.");
+                        } else {
+                            arreglo[100 - numero_registros] = nombre_completo;
+                            numero_registros--;
+                            System.out.println("Cliente registrado exitosamente.");
+                        }
+                    } else {
+                        System.out.println("No se puede registrar un nuevo cliente.");
+                    }
+                    break;
+
 
                     case 2:
-                        for (int i = 0; i < 100 - numero_registros; i++) {
-                            System.out.println("--> "+arreglo[i]);
-                        }
-                        break;
+
+                    for (int i = 0; i < 100 - numero_registros; i++) {
+                        System.out.println("--> "+arreglo[i]);
+                    }
+                    break;
 
                     case 3:
-                        System.out.print("--->   Ingrese el nombre del cliente a editar: ");
+
+                    if (numero_registros < 100) {
+                        System.out.print("--->   Ingrese el nombre completo del cliente a editar: ");
                         entrada.nextLine(); 
-                        String nombre_editar = entrada.nextLine();
-                        int editar = 1;
+                        String editar_nombre = entrada.nextLine();
+                        boolean encontrado = false;
+
                         for (int i = 0; i < 100 - numero_registros; i++) {
-                            if (arreglo[i].equals(nombre_editar)) {
-                                editar = i;
+                            if (arreglo[i].equals(editar_nombre)) {
+                                System.out.print("--->   Ingrese el nuevo nombre completo: ");
+                                arreglo[i] = entrada.nextLine();
+                                System.out.println("Cliente editado exitosamente.");
+                                encontrado = true;
                                 break;
                             }
                         }
-                        if (editar != 1) {
-                            System.out.print("--->   Ingrese el nuevo nombre completo: ");
-                            arreglo[editar] = entrada.nextLine();
-                            System.out.println("Cliente editado exitosamente.");
-                        } else {
+
+                        if (!encontrado) {
                             System.out.println("Nombre de cliente no encontrado.");
                         }
-                        
-                        break;
+                    } else {
+                        System.out.println("No hay clientes registrados.");
+                    }
+                    break;
 
                     case 4:
-                        System.out.print("--->   Ingrese el nombre del cliente a eliminar: ");
-                        entrada.nextLine();
-                        String nombre_eliminar = entrada.nextLine();
-                        int eliminar = 1;
-                        for (int i = 0; i < 100 - numero_registros; i++) {
-                            if (arreglo[i].equals(nombre_eliminar)) {
-                                eliminar = i;
-                                break;
-                            }
+
+                    System.out.print("--->   Ingrese el nombre del cliente a eliminar: ");
+                    entrada.nextLine();
+                    String nombre_eliminar = entrada.nextLine();
+                    int eliminar = -1;
+                    for (int i = 0; i < 100 - numero_registros; i++) {
+                        if (arreglo[i].equals(nombre_eliminar)) {
+                            eliminar = i;
+                            break;
                         }
-                        if (eliminar != 1) {
-                            for (int i = eliminar; i < 100 - numero_registros - 1; i++) {
-                                arreglo[i] = arreglo[i + 1];
-                            }
-                            arreglo[100 - numero_registros - 1] = null;
-                            numero_registros++;
-                            System.out.println("Cliente eliminado exitosamente.");
-                        } else {
-                            System.out.println("Nombre de cliente no encontrado.");
+                    }
+                    if (eliminar != -1) {
+                        for (int i = eliminar; i < 100 - numero_registros - 1; i++) {
+                            arreglo[i] = arreglo[i + 1];
                         }
-                        
-                        break;
+                        arreglo[100 - numero_registros - 1] = null;
+                        numero_registros++;
+                        System.out.println("Cliente eliminado exitosamente.");
+                    } else {
+                        System.out.println("Nombre de cliente no encontrado.");
+                    }
+                    
+                    break;
 
                     case 5:
-                        System.out.println("Saliendo...");
-                        break;
+                    
+                    System.out.println("Saliendo...");
+                    break;
 
                     default:
-                        System.out.println("Opcion invalida.");
-                        break;
+                    System.out.println("Opcion invalida.");
+                    break;
                 }
             } while (opcion != 5);
         }
