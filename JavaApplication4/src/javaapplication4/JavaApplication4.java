5785b33f33032cdaa66f99e946f9e2cc8b6527b1
Nicolas Tapasco <tapasconicolas13@gmail.com>
2024-02-12 13:05:18 -0500

    agrupacion de archivos en carpeta Netbeens

--- JavaApplication4/src/javaapplication4/JavaApplication4.java
+++
@@ -1,8 +0,0 @@
-package javaapplication4;
-public class JavaApplication4 {
-
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-}
---
+++ Netbeens/Tablas/src/principal/Principal.java
@@ -0,0 +1,12 @@
+package principal;
+
+public class Principal {
+    public static void main(String[] args) {
+        
+        //TablaBotones ventana01 = new TablaBotones();
+        TablaBasica ventana = new TablaBasica();
+        //Alerta vtn = new Alerta();
+        //Aviso ventanaAviso = new Aviso();
+        
+    }
+}
